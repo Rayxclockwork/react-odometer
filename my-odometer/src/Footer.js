c9b4be68d885a41b3bb65080ef0057e5e2ffb83f
RAVEN DELANEY <ravend17@gmail.com>
2020-01-28 17:33:04 -0800

    working on props inheritance

---
+++ my-odometer/src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+    render() {
+        return (
+			<>
+           <h2>© Raven Delaney 2020</h2>
+           <p>Code Fellows</p>
+           </>
+        )
+    }
+}
+
+export default Footer;
